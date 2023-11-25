e682366467330f4f1463cd08a4b9a1760b136a78
GUchechi <godswilluchendu7@gmail.com>
2023-11-25 22:49:15 +0100

    nearbyJobs

---
+++ app/job-details/[id].js
@@ -0,0 +1,39 @@
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from "../../components";
+import { COLORS, icons, SIZES } from "../../constants";
+import useFetch from "../../hook/useFetch";
+
+export default function JobDetails() {
+  const params = useSearchParams();
+  const router = useRouter();
+
+  const { data, isLoading, error, refetch } = useFetch("job-details", {
+    job_id: params.id,
+  });
+
+  // const [activeTab, setActiveTab] = 
+  const [first, setfirst] = useState(second)(tabs[0]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  return (
+    <View>
+      <Text>JobDetails</Text>
+    </View>
+  );
+}
