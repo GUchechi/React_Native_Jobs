e98878b23077c76c5abae6ab5b24863305122ba3
GUchechi <godswilluchendu7@gmail.com>
2023-11-26 23:10:32 +0100

    jobDetails

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -30,6 +30,13 @@ export default function JobDetails() {
   // const [activeTab, setActiveTab] = useState(tabs[0]);
   const [refreshing, setRefreshing] = useState(false);
 
+  // Refresh
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    refetch();
+    setRefreshing(false);
+  }, []);
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -50,6 +57,40 @@ export default function JobDetails() {
           headerTitle: "",
         }}
       />
+
+      <>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          {isLoading ? (
+            <ActivityIndicator size="large" color={COLORS.primary} />
+          ) : error ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data available</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+
+              {/* <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+
+              {displayTabContent()} */}
+            </View>
+          )}
+        </ScrollView>
+      </>
     </SafeAreaView>
   );
 }
